c89208c4b75105ce155120a44c0cc34110aadb5a
Nicholas <njhylands@gmail.com>
2018-06-28 11:29:20 -0400

    first commit, following the vulkan tutorial

---
+++ VulkanTest/vulcanFirstTime.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifndef VULC_FIRST_TIME
+
+#define VULC_FIRST_TIME
+
+#include "stdafx.h"
+
+// this lets us invlude GLFW definitions, and automatically load the vulkan headers with it.
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <functional>
+#include <cstdlib>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <fstream>
+#include <glm/glm.hpp>
+#include <array>
+
+#endif
